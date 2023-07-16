a85275f92dc9a9c675fef92d75d6ec6370b64620
RayAgu <aguye.raymond@gmail.com>
2023-07-17 00:21:34 +0100

    user-verification commit

---
+++ routers/userRouter.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const {signUp,verifyEmail, resendVerificationEmail, signIn, signOut } = require("../controllers/usercontroller")
+
+router.route("/user/sign-up").post(signUp);
+
+router.route("/user/verify-email/:token").get(verifyEmail);
+
+router.route("/user/resend-verification-email").post(resendVerificationEmail);
+
+router.route("/user/sign-in").post(signIn);
+
+router.route("/user/sign-out").post(signOut)
+
+
+module.exports = router;
